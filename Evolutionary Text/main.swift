12cbdeef5665e9971acd84a9ec9d17ba44979d11
Can <kralciplak9@gmail.com>
2019-03-01 22:40:06 +0300

    Initial Commit

---
+++ Evolutionary Text/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Evolutionary Text
+//
+//  Created by Can Balkaya on 3/1/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, welcome to ET!")
