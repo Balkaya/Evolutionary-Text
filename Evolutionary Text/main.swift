29e2ab35a76fe4fb67837c0d1098a4f34fbe38cf
Can Balkaya <canbalkya@gmail.com>
2019-12-20 19:34:55 +0300

    Developed some things

--- Evolutionary Text/main.swift
+++ Evolutionary Text/main.swift
@@ -13,19 +13,49 @@ class EvolutionaryText {
     var oldGenerations = [[String]]()
     var answerFunction = { readLine() }
     
-    func process(text: String) -> String {
-        newGeneration = [text, text, text, text, text, text, text]
-        return text
+    func randomString(text: String) -> String {
+        let letters: NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let len = UInt32(letters.length)
+        var randomString = ""
+        
+        for _ in 0...text.count {
+            let random = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(random))
+            
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
+    
+    func process(_ answer: String, text: String) -> String {
+        guard let answerInt = Int(answer) else {
+            return "Error"
+        }
+        
+        if text.components(separatedBy: .whitespaces).count > 1 {
+            return "Please, just enter a sentence."
+        } else {
+//            while sentence == text {
+            newGeneration.removeAll()
+                for i in 0...text.count {
+                    newGeneration[i] += randomString(text: text)
+                }
+            print(newGeneration)
+//            }
+            
+            return text
+        }
     }
     
     func start() {
         print("Welcome to Evolutionary Text!")
         
         var text = ""
-        print("Please enter a text: ", terminator: "")
+        print("Please enter a sentence: ", terminator: "")
         
         if let answer = answerFunction() {
-            let response = process(text: text)
+            let response = process(answer, text: text)
             print(response)
         }
     }
